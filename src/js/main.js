1ce0dfb59e1ecc23046d17c6c0db79334ba90630
shahbozshirinboyev <106464818+shahbozshirinboyev@users.noreply.github.com>
2024-08-15 14:37:30 +0500

    add API, Loader and Error alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import '../css/main.css';
+import request from './request';
+
+const API = 'https://restcountries.com/v3.1/all';
+request(API).then((data) => {
+    console.log(data)
+}).catch((err) => {
+    alert(err.message);
+})
