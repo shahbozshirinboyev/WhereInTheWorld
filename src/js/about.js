10a0385b8522d4664d876822869758b389cd2aa0
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-08-15 22:47:32 +0500

    Set about.html

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,15 @@
 import '../css/main.css';
+import './mode';
+import request from './request'
+import { createCountrieInfo } from './updateUI';
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const country = urlParams.get('country');
+const countryAPI = `https://restcountries.com/v3.1/name/${country}`;
+
+request(countryAPI).then((data) => {
+    createCountrieInfo(data[0]);
+}).catch((err) => {
+    alert(err.message)
+})
--- src/js/updateUI.js
+++ src/js/updateUI.js
@@ -14,7 +14,7 @@ export const createCountries = (countries) => {
         li.classList.add('cards__item');
         li.innerHTML = 
         `
-        <a href="./about.html">
+        <a href="./about.html?country=${commonName}">
                 <img src=${flag} alt="algeria-flag" width="267" height="160">
                 <div class="cards__item-inner">
                     <h3 class="cards__title">${commonName}</h3>
@@ -41,4 +41,129 @@ export const createCountries = (countries) => {
                         </div>
                     </a>
 </li>
+*/
+
+// About UI
+const countryInfoEl = document.querySelector('.country-info');
+export const createCountrieInfo = (country) => {
+    const {population, borders, capital, flags, name, region, tld, currencies, languages, subregion} = country
+    const nativeName = Object.values(name.nativeName)[0].official;
+    const currency = Object.values(currencies)[0].name;
+    const language = Object.values(languages);
+
+    countryInfoEl.innerHTML = 
+    `    
+                <img class="country-info__img" src=${flags.svg} alt="germany-flag" width="560"
+                    height="400" />
+                <div class="country-info__content">
+                    <h2>${name.common}</h2>
+                    <ul class="country-info__list">
+                        <li class="country-info__item">
+                            <p class="name">
+                                Native Name:
+                                <span>${nativeName}</span>
+                            </p>
+                            <p class="population">
+                                Population:
+                                <span>${population}</span>
+                            </p>
+                            <p class="region">
+                                Region:
+                                <span>${region}</span>
+                            </p>
+                            <p class="sub-region">
+                                Sub Region:
+                                <span>${subregion}</span>
+                            </p>
+                            <p class="capital">
+                                Capital:
+                                <span>${capital}</span>
+                            </p>
+                        </li>
+                        <li class="country-info__item">
+                            <p class="name">
+                                Top Level Domain:
+                                <span>${tld}</span>
+                            </p>
+                            <p class="population">
+                                Currencies:
+                                <span>${currency}</span>
+                            </p>
+                            <p class="region">
+                                Languages:
+                                <span>${language}</span>
+                            </p>
+                        </li>
+                    </ul>
+
+                    <div class="country-info__borders">
+                        <h3>Border Countries:</h3>
+                        ${borders ? borders.map((border) => {
+                            return `<a href="./about.html?country=${border}">${border}</a>`
+                        }) : "No Borders"}
+                    </div>
+                </div>
+            
+    `;
+};
+
+/*
+
+                <img class="country-info__img" src="./images/jpg/germany-flag.jpg" alt="germany-flag" width="560"
+                    height="400" />
+                <div class="country-info__content">
+                    <h2>Germany</h2>
+                    <ul class="country-info__list">
+                        <li class="country-info__item">
+                            <p class="name">
+                                Native Name
+                                <span>Germany</span>
+                            </p>
+                            <p class="population">
+                                Population:
+                                <span>81,770,900</span>
+                            </p>
+                            <p class="region">
+                                Region:
+                                <span>Europe</span>
+                            </p>
+                            <p class="sub-region">
+                                Sub Region:
+                                <span>Western Europe</span>
+                            </p>
+                            <p class="capital">
+                                Capital:
+                                <span>Berlin</span>
+                            </p>
+                        </li>
+                        <li class="country-info__item">
+                            <p class="name">
+                                Top Level Domain:
+                                <span>.de</span>
+                            </p>
+                            <p class="population">
+                                Currencies:
+                                <span>Euro</span>
+                            </p>
+                            <p class="region">
+                                Languages:
+                                <span>Dutch, German</span>
+                            </p>
+                        </li>
+                    </ul>
+
+                    <div class="country-info__borders">
+                        <h3>Border Countries:</h3>
+                        <a href="#">France</a>
+                        <a href="#">Belgium</a>
+                        <a href="#">Netherlands</a>
+                        <a href="#">Denmark</a>
+                        <a href="#">Poland</a>
+                        <a href="#">Szech</a>
+                        <a href="#">Austria</a>
+                        <a href="#">Switzerland</a>
+                        <a href="#">Luxembourg</a>
+                    </div>
+                </div>
+            
 */
